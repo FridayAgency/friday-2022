8253b14162bca39be4e248dd5295275a87fa8a81
friday <support@friday.ie>
2022-07-19 12:03:20 +0100

    add custom header to revision query

--- queries/pages/queryPageByUri.ts
+++ queries/pages/queryPageByUri.ts
@@ -1,60 +1,63 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
-import { SeoFragment } from "../fragments/seo";
-import { PagePanelsFragment } from "../fragments/pagePanels";
+import { SeoFragment } from '../fragments/seo';
+import { PagePanelsFragment } from '../fragments/pagePanels';
 
 export const GET_PAGE_BY_URI = gql`
-	query PageByUri($uri: ID!, $idType: PageIdType!, $asPreview: Boolean = false) {
-		page(id: $uri, idType: $idType, asPreview: $asPreview) {
-			id
-			slug
-			title
-			uri
-			standardPageHeader {
-				pageCustomTitle
-			}
-			contactPage {
-				contactCompanyAddress
-				contactSecondaryContent
-				fieldGroupName
-				googleMap {
-					latitude
-					longitude
-				}
-			}
-			isPreview
-			revisions(where: { orderby: { field: MODIFIED, order: DESC } }, first: 1) {
-				nodes {
-					id
-					slug
-					title
-					uri
-					isPreview
-					contactPage {
-						contactCompanyAddress
-						contactSecondaryContent
-						fieldGroupName
-						googleMap {
-							latitude
-							longitude
-						}
-					}
-					pagePanels {
-						...PagepanelsFragment
-					}
-					seo {
-						...PostTypeSEOFragment
-					}
-				}
-			}
-			pagePanels {
-				...PagepanelsFragment
-			}
-			seo {
-				...PostTypeSEOFragment
-			}
-		}
-	}
-	${SeoFragment}
-	${PagePanelsFragment}
+  query PageByUri($uri: ID!, $idType: PageIdType!, $asPreview: Boolean = false) {
+    page(id: $uri, idType: $idType, asPreview: $asPreview) {
+      id
+      slug
+      title
+      uri
+      standardPageHeader {
+        pageCustomTitle
+      }
+      contactPage {
+        contactCompanyAddress
+        contactSecondaryContent
+        fieldGroupName
+        googleMap {
+          latitude
+          longitude
+        }
+      }
+      isPreview
+      revisions(where: { orderby: { field: MODIFIED, order: DESC } }, first: 1) {
+        nodes {
+          id
+          slug
+          title
+          uri
+          standardPageHeader {
+            pageCustomTitle
+          }
+          isPreview
+          contactPage {
+            contactCompanyAddress
+            contactSecondaryContent
+            fieldGroupName
+            googleMap {
+              latitude
+              longitude
+            }
+          }
+          pagePanels {
+            ...PagepanelsFragment
+          }
+          seo {
+            ...PostTypeSEOFragment
+          }
+        }
+      }
+      pagePanels {
+        ...PagepanelsFragment
+      }
+      seo {
+        ...PostTypeSEOFragment
+      }
+    }
+  }
+  ${SeoFragment}
+  ${PagePanelsFragment}
 `;
