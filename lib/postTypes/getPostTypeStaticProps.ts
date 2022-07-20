065eabde6c28f276f09a65da73ff895e1e2b103c
friday <support@friday.ie>
2022-07-20 10:10:42 +0100

    add 404 for dynamic pages

--- lib/postTypes/getPostTypeStaticProps.ts
+++ lib/postTypes/getPostTypeStaticProps.ts
@@ -1,11 +1,11 @@
-import { getAllMenus } from "../menus/getAllMenus";
-import type { Page, Menu, Post, Project, GfForm } from "../../types/graphql";
-import { getPostTypeByUri } from "./getPostTypeByUri";
-import { FooterDetails, getFooterDetails } from "../footer/getFooterDetails";
-import getGravityFormById from "../gravityForms.ts/getGravityFormById";
-import { getAllPostsByCategory } from "../posts/getPostsByCategory";
-import { getAllPosts } from "../posts/getAllPosts";
-import { getAllProjects } from "../projects/getAllProjects";
+import { getAllMenus } from '../menus/getAllMenus';
+import type { Page, Menu, Post, Project, GfForm } from '../../types/graphql';
+import { getPostTypeByUri } from './getPostTypeByUri';
+import { FooterDetails, getFooterDetails } from '../footer/getFooterDetails';
+import getGravityFormById from '../gravityForms.ts/getGravityFormById';
+import { getAllPostsByCategory } from '../posts/getPostsByCategory';
+import { getAllPosts } from '../posts/getAllPosts';
+import { getAllProjects } from '../projects/getAllProjects';
 
 /**
  * Get the page static props
@@ -15,56 +15,64 @@ import { getAllProjects } from "../projects/getAllProjects";
  */
 
 export type PostTypeStaticProps = {
-	props: {
-		menus: Array<Menu>;
-		content: Page | Post | Project;
-		gfForm?: GfForm;
-		posts?: Array<Post>;
-		footerData: FooterDetails;
-		projects?: Array<Project>;
-	};
+  props: {
+    menus?: Array<Menu>;
+    content?: Page | Post | Project;
+    gfForm?: GfForm;
+    posts?: Array<Post>;
+    footerData?: FooterDetails;
+    projects?: Array<Project>;
+  };
+  notFound?: true;
 };
 
 export const getPostTypeStaticProps = async (
-	uri: string,
-	postType: "page" | "post" | "project"
+  uri: string,
+  postType: 'page' | 'post' | 'project'
 ): Promise<PostTypeStaticProps> => {
-	const [{ menus }, content, footerData] = await Promise.all([
-		getAllMenus(),
-		getPostTypeByUri(uri, false, postType),
-		getFooterDetails(),
-	]);
+  const [{ menus }, content, footerData] = await Promise.all([
+    getAllMenus(),
+    getPostTypeByUri(uri, false, postType),
+    getFooterDetails(),
+  ]);
 
-	let gfForm: GfForm = null;
-	let projects: Array<Project> = null;
+  let gfForm: GfForm = null;
+  let projects: Array<Project> = null;
 
-	if (uri === "/contact/") {
-		gfForm = await getGravityFormById(1);
-	}
+  if (uri === '/contact/') {
+    gfForm = await getGravityFormById(1);
+  }
 
-	let posts: Array<Post> = null;
-	if (uri === "/careers/") {
-		posts = await getAllPostsByCategory("careers");
-	}
+  let posts: Array<Post> = null;
+  if (uri === '/careers/') {
+    posts = await getAllPostsByCategory('careers');
+  }
 
-	if (uri === "/") {
-		const data = await getAllPosts(2);
-		posts = data.posts;
-	}
+  if (uri === '/') {
+    const data = await getAllPosts(2);
+    posts = data.posts;
+  }
 
-	if (uri === "/work/") {
-		const data = await getAllProjects();
-		projects = data?.projects;
-	}
+  if (uri === '/work/') {
+    const data = await getAllProjects();
+    projects = data?.projects;
+  }
 
-	return {
-		props: {
-			menus,
-			content,
-			gfForm,
-			posts,
-			footerData,
-			projects,
-		},
-	};
+  if (!content) {
+    return {
+      notFound: true,
+      props: {},
+    };
+  }
+
+  return {
+    props: {
+      menus,
+      content,
+      gfForm,
+      posts,
+      footerData,
+      projects,
+    },
+  };
 };
